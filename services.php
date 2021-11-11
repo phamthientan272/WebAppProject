caacf38c63a1b477ef9283d1edc62cf534351e9f
yojiyeo <chyeo42@gmail.com>
2021-11-12 00:09:10 +0800

    Added flavor text + beautify some code

--- services.php
+++ services.php
@@ -2,71 +2,76 @@
 <html lang="en">
 
 <head>
-  <title>Our Services</title>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="stylesheet.css">
+	<title>Our Services</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="stylesheet.css">
 </head>
 
 <body>
-  <div id="wrapper2">
-    <header>
-      <?php include 'header.php' ?>
-    </header>
-
-    <div class="box_img">
-      <img src="assets/services.jpg">
-      <div class="centered">Our Services</div>
-    </div>
-
-    <h2 class="service_text">Therapy & Counseling</h2>
-    <div id="box2">
-      <div class="image_box">
-        <img class="img2" src="assets/doctor1.jpg" alt="test2">
-      </div>
-      <div class="paragraph_box">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat.<br><br>Duration: 50 minutes<br>Price: $50 (Payment to be collected at the day itself)</p>
-      </div>
-      <div class="button_box">
-        <a href="booking_service.php"><button class="paragraph_btn">Book Now</button></a>
-      </div>
-    </div>
-
-    <h2 class="service_text1">Yoga Class</h2>
-    <div id="box2">
-      <div class="image_box">
-        <img class="img2" src="assets/yogi1.jpg" alt="test2">
-      </div>
-      <div class="paragraph_box">
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat.<br><br>Duration: 50 minutes<br>Price: $50 (Payment to be collected at the day itself)</p>
-      </div>
-      <div class="button_box">
-       <a href="booking_service.php"><button class="paragraph_btn">Book Now</button></a>
-      </div>
-    </div>
-
-    <h2 class="service_text1">Massage</h2>
-    <div id="box2">
-      <div class="image_box">
-        <img class="img2" src="assets/psych1.jpg" alt="test2">
-      </div>
-      <div class="paragraph_box">
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat.<br><br>Duration: 50 minutes<br>Price: $50 (Payment to be collected at the day itself)</p>
-      </div>
-      <div class="button_box">
-        <a href="booking_service.php"><button class="paragraph_btn">Book Now</button></a>
-      </div>
-    </div>
-    <br>
-    
-    <footer>
-      <?php include 'footer.php' ?>
-    </footer>
-  </div>
+	<div id="wrapper2">
+		<header>
+			<?php include 'header.php' ?>
+		</header>
+		<div class="box_img">
+			<img src="assets/services.jpg">
+			<div class="centered">Our Services</div>
+		</div>
+		<h2 class="service_text">Therapy & Counseling</h2>
+		<div id="box2">
+			<div class="image_box">
+				<img class="img2" src="assets/doctor1.jpg" alt="test2">
+			</div>
+			<div class="paragraph_box">
+				<p>Do you need someone to listen to? Talk about anything and everything? Our liscensed therapist will be able to help you out with your problems and allow you to process what you need to understand it, without judgment whatsoever.
+					<br>
+					<br>Duration: 50 minutes
+					<br>Price: $50 (Payment to be collected at the day itself)</p>
+			</div>
+			<div class="button_box">
+				<a href="booking_service.php">
+					<button class="paragraph_btn">Book Now</button>
+				</a>
+			</div>
+		</div>
+		<h2 class="service_text1">Yoga Class</h2>
+		<div id="box2">
+			<div class="image_box">
+				<img class="img2" src="assets/yogi1.jpg" alt="test2">
+			</div>
+			<div class="paragraph_box">
+				<p>Kick back and relax with our yoga package, designed to ensure you have a good time while sweating out all the stress and troubles away. Our experienced yoga instructor will be taking the class, so fret not if this is your first time!
+					<br>
+					<br>Duration: 50 minutes
+					<br>Price: $50 (Payment to be collected at the day itself)</p>
+			</div>
+			<div class="button_box">
+				<a href="booking_service.php">
+					<button class="paragraph_btn">Book Now</button>
+				</a>
+			</div>
+		</div>
+		<h2 class="service_text1">Massage</h2>
+		<div id="box2">
+			<div class="image_box">
+				<img class="img2" src="assets/psych1.jpg" alt="test2">
+			</div>
+			<div class="paragraph_box">
+				<p>Those knots in your shoulders and calves won't go away by themselves. Let our physiotherapst give you a sports massage, designed to help relieve tension and increase happiness.
+					<br>
+					<br>Duration: 50 minutes
+					<br>Price: $50 (Payment to be collected at the day itself)</p>
+			</div>
+			<div class="button_box">
+				<a href="booking_service.php">
+					<button class="paragraph_btn">Book Now</button>
+				</a>
+			</div>
+		</div>
+		<br>
+		<footer>
+			<?php include 'footer.php' ?>
+		</footer>
+	</div>
 </body>
+
 </html>
--- team.php
+++ team.php
@@ -2,72 +2,76 @@
 <html lang="en">
 
 <head>
-  <title>Our Team</title>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="stylesheet.css">
+	<title>Our Team</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="stylesheet.css">
 </head>
 
 <body>
-  <div id="wrapper1">
-    <header>
-      <?php include 'header.php' ?>
-    </header>
-
-    <div class="box_img">
-      <img src="assets/team.jpg">
-      <div class="centered">Our Team</div>
-    </div>
-
-    <h2 class="service_text">Dr. Meredith Grey</h2>
-    <div id="box2">
-      <div class="image_box">
-        <img class="img2" src="assets/doctor.jpg" alt="test2">
-      </div>
-      <div class="paragraph_box">
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat.<br><br>Education: MBBS, M Med (Psychiatry), FAMS<br>Experience: 10 Years (as of 2021)</p>
-      </div>
-      <div class="button_box">
-        <a href="booking_service.php"><button class="paragraph_btn">Book Now</button></a>
-      </div>
-    </div>
-
-    <h2 class="service_text1">Ms. Joaquim Blue</h2>
-    <div id="box2">
-      <div class="image_box">
-        <img class="img2" src="assets/yogi.jpg">
-      </div>
-      <div class="paragraph_box">
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. <br><br>Education: BSc Sports & Exercise Science<br>Experience: 13 Years (as of 2021)</p>
-      </div>
-      <div class="button_box">
-        <a href="booking_service.php"><button class="paragraph_btn">Book Now</button></a>
-      </div>
-    </div>
-
-    <h2 class="service_text1">Ms. Jane Doe</h2>
-    <div id="box2">
-      <div class="image_box">
-        <img class="img2" src="assets/psych.jpg" alt="test2">
-      </div>
-      <div class="paragraph_box">
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. <br><br>Education: Master of Musculoskeletal & Sports Physiotherapist (Aus)<br>Experience: 7 Years (as of 2021)</p>
-      </div>
-      <div class="button_box">
-        <a href="booking_service.php"><button class="paragraph_btn">Book Now</button></a>
-      </div>
-    </div>
-    <br>
-
-    <footer>
-      <?php include 'footer.php' ?>
-    </footer>
-  </div>
+	<div id="wrapper1">
+		<header>
+			<?php include 'header.php' ?>
+		</header>
+		<div class="box_img">
+			<img src="assets/team.jpg">
+			<div class="centered">Our Team</div>
+		</div>
+		<h2 class="service_text">Dr. Meredith Grey</h2>
+		<div id="box2">
+			<div class="image_box">
+				<img class="img2" src="assets/doctor.jpg" alt="test2">
+			</div>
+			<div class="paragraph_box">
+				<p>Meredith prides himself as a doctor first and foremost. The treatment of his patients and their troubles are the first thing on his mind at work. Having start work at a tender age at a local hospital, Meredith has blossomed into a seasoned doctor with a plethora of experience.
+					<br>
+					<br>Education: MBBS, M Med (Psychiatry), FAMS
+					<br>Experience: 10 Years (as of 2021)</p>
+			</div>
+			<div class="button_box">
+				<a href="booking_service.php">
+					<button class="paragraph_btn">Book Now</button>
+				</a>
+			</div>
+		</div>
+		<h2 class="service_text1">Ms. Joaquim Blue</h2>
+		<div id="box2">
+			<div class="image_box">
+				<img class="img2" src="assets/yogi.jpg">
+			</div>
+			<div class="paragraph_box">
+				<p>Yoga has always been an integral part of Joaquim's life. From a side hobby to a full time job, Joaquim enjoys what she does for a living and embraces the power of yoga. She hopes to leverage yoga as a tool to help instill a healthy lifestyle to the youths of Singapore.
+					<br>
+					<br>Education: BSc Sports & Exercise Science
+					<br>Experience: 13 Years (as of 2021)</p>
+			</div>
+			<div class="button_box">
+				<a href="booking_service.php">
+					<button class="paragraph_btn">Book Now</button>
+				</a>
+			</div>
+		</div>
+		<h2 class="service_text1">Ms. Jane Doe</h2>
+		<div id="box2">
+			<div class="image_box">
+				<img class="img2" src="assets/psych.jpg" alt="test2">
+			</div>
+			<div class="paragraph_box">
+				<p>Jane realised her true calling as a physiotherapist when she realise her career as a professional swimmer is not a long term option. With experience as a national athlete, you can rest assure knowing Jane is an ex-athlete herself and understands what they need too.
+					<br>
+					<br>Education: Master of Musculoskeletal & Sports Physiotherapist (Aus)
+					<br>Experience: 7 Years (as of 2021)</p>
+			</div>
+			<div class="button_box">
+				<a href="booking_service.php">
+					<button class="paragraph_btn">Book Now</button>
+				</a>
+			</div>
+		</div>
+		<br>
+		<footer>
+			<?php include 'footer.php' ?>
+		</footer>
+	</div>
 </body>
 
 </html>
